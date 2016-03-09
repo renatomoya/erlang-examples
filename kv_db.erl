1d8ea713de1a9f1291170b6cc69374fd9221d6ef
Renato Moya <renato.moya@groupon.com>
2016-03-08 23:10:40 -0800

    Finish implementation of `kv_db` and `kv_db_tests`

--- kv_db.erl
+++ kv_db.erl
@@ -5,13 +5,31 @@
 -type err() :: {'error', string()}.
 
 -export_type([db/0, results/0, err/0]).
--export([new/0]).
+-export([new/0, put/3, get/2, delete/2]).
 
 -spec new() -> db().
 new() -> [].
 
-% -spec put(Key::atom(), Value::term(), Db::db()) -> results() | err().
+-spec put(Key::atom(), Value::term(), Db::db()) -> results() | err().
+put(Key, Value, []) when is_atom(Key) ->
+  [{Key, Value}];
+put(Key, Value, [{Key, _} | Db]) when is_atom(Key) ->
+  [{Key, Value} | Db];
+put(Key, Value, [Current | Db]) when is_atom(Key) ->
+  [Current | put(Key, Value, Db)].
 
-% -spec get(Key::atom(), Db::db()) -> term() | err().
+-spec get(Key::atom(), Db::db()) -> term() | err().
+get(Key, []) when is_atom(Key) ->
+  {error, "Key not found: " ++ atom_to_list(Key)};
+get(Key, [{Key, Value} | _]) when is_atom(Key) ->
+  Value;
+get(Key, [_ | Db]) when is_atom(Key) ->
+  get(Key, Db).
 
-% -spec delete(Key::atom(), Db::db()) -> (results() | nil()) | err().
+-spec delete(Key::atom(), Db::db()) -> (results() | nil()) | err().
+delete(Key, []) ->
+  {error, "Key not found: " ++ atom_to_list(Key)};
+delete(Key, [{Key, _Value} | Db]) ->
+  Db;
+delete(Key, [Tuple | Db]) ->
+  [Tuple | delete(Key, Db)].
--- kv_db_tests.erl
+++ kv_db_tests.erl
@@ -4,3 +4,47 @@
 
 new_test() ->
   ?assertEqual([], kv_db:new()).
+
+put_test() ->
+  Db0 = kv_db:new(),
+  Db1 = kv_db:put(color, "red", Db0),
+
+  ?assertEqual([{color, "red"}], Db1),
+
+  Db2 = kv_db:put(color, "blue", Db1),
+
+  ?assertEqual([{color, "blue"}], Db2),
+
+  Db3 = kv_db:put(number, 7, Db2),
+
+  ?assertEqual([{color, "blue"},{number, 7}], Db3).
+
+get_test() ->
+  Db0 = kv_db:new(),
+
+  ?assertEqual({error, "Key not found: color"}, kv_db:get(color, Db0)),
+
+  Db1 = kv_db:put(color, "red", Db0),
+
+  ?assertEqual("red", kv_db:get(color, Db1)).
+
+delete_test() ->
+  Db0 = kv_db:new(),
+
+  ?assertEqual({error, "Key not found: color"}, kv_db:delete(color, Db0)),
+
+  Db1 = kv_db:put(color, "red", Db0),
+  Db2 = kv_db:put(number, 7, Db1),
+
+  ?assertEqual([{color, "red"}, {number, 7}], Db2),
+
+  Db3 = kv_db:delete(color, Db2),
+
+  ?assertEqual([{number, 7}], Db3),
+
+  Db4 = kv_db:put(color, "red", Db3),
+
+  Db5 = kv_db:delete(number, Db4),
+
+  ?assertEqual([{color, "red"}], Db5),
+  ?assertEqual([], kv_db:delete(color, Db5)).
